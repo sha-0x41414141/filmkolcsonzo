56618ea5d7091bc46d081cc0db596d263bb0280b
sha-0x41414141 <87914329+sha-0x41414141@users.noreply.github.com>
2024-11-15 09:21:33 +0100

    film

--- resources/views/films/index.blade.php
+++ resources/views/films/index.blade.php
@@ -5,12 +5,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Films</title>
 </head>
+<style>
+    td, th{
+        padding: 15px;
+    }
+    table, td, th{
+        border: 1px solid black;
+        border-collapse: collapse;
+        text-align: center;
+    }
+    table{
+        margin: auto;
+    }
+</style>
 <body>
-    <h1>Films</h1>
+    <h1 style="text-align: center">Films</h1>
     <table>
         <tr>
             <th>Film title</th>
+            <th>Film director</th>
+            <th>Film year</th>
+            <th>Rent</th>
         </tr>
+        @foreach($films as $film)
+            <tr>
+                <td>{{$film->film_title}}</td>
+                <td>{{$film->film_director}}</td>
+                <td>{{$film->film_year}}</td>
+                <td><button type="submit">Rent</button></td>
+            </tr>
+        @endforeach
     </table>
 </body>
 </html>
