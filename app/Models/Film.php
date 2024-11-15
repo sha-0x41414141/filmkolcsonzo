94494f16c5d1df9466a04c72537bf1b859ea816c
sha-0x41414141 <87914329+sha-0x41414141@users.noreply.github.com>
2024-11-15 08:44:43 +0100

    Models

--- app/Models/Film.php
+++ app/Models/Film.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Film extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['film_title', 'film_director', 'genre_id', 'film_year'];
+
+    public function genre()
+    {
+        return $this->belongsTo(Genre::class);
+    }
+
+    public function rents()
+    {
+        return $this->hasMany(Rent::class);
+    }
 }
--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Genre extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['genre_name'];
+
+    public function films()
+    {
+        return $this->hasMany(Film::class);
+    }
 }
